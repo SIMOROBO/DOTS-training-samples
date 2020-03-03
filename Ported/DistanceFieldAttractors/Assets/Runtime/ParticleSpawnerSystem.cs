9ea43d0fc6a93ccd0a6679f7ce3b2ae258cd019f
Hideya Sugiyama <hsugiyama@eidosmontreal.com>
2020-03-03 14:04:03 -0500

    added particle spawner

---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ParticleSpawnerSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+[GenerateAuthoringComponent]
+public struct ParticlSpawner : IComponentData
+{
+    public int Count;
+    public Entity Prefab;
+}
+
+public class ParticleSpawnerSystem : JobComponentSystem
+{
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        Entities.WithStructuralChanges().WithoutBurst().ForEach((in ParticlSpawner spawner) =>
+        {
+            var particls = EntityManager.Instantiate(spawner.Prefab, spawner.Count, Allocator.Temp);
+            var positions = new NativeArray<float3>(spawner.Count, Allocator.Temp);
+            //GeneratePoints.RandomPointsInUnitSphere(positions);
+            //positions.Dispose();
+            particls.Dispose();
+        }).Run();
+        return default;
+    }
+}
