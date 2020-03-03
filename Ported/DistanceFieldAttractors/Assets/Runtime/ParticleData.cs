0e18d7665efe407c159e9e5985f08471960d6217
Yan Verde <yverde@eidosmontreal.com>
2020-03-03 12:54:10 -0500

    add particle data and folder structure

---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ParticleData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public class ParticleData : IComponentData
+{
+	public Vector3 position;
+	public Vector3 velocity;
+	public Vector4 color;
+}
