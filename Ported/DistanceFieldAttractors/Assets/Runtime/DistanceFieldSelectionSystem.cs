1eedbe602d05deb7e985a643b092bd137d91fe1a
Yan Verde <yverde@eidosmontreal.com>
2020-03-03 17:03:36 -0500

    Mode selection system

---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/DistanceFieldSelectionSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+
+public enum DistanceFieldModels
+{
+    SpherePlane,
+    Metaballs,
+    SpinMixer,
+    SphereField,
+    FigureEight,
+    PerlinNoise
+}
+
+[GenerateAuthoringComponent]
+public struct DistanceFieldData : IComponentData
+{
+    public DistanceFieldModels Model;
+    public double ElapsedTime;
+    public KeyCode ChangeModeKey;
+}
+
+[AlwaysSynchronizeSystem]
+public class DistanceFieldSelectionSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var deltatime = Time.DeltaTime;
+        
+        Entities.WithoutBurst().ForEach((ref DistanceFieldData distanceField) => {
+
+            var mode = distanceField.Model;
+            var currentMode = (int)mode;
+            var time = distanceField.ElapsedTime + deltatime;
+
+            if (time >= 10 || Input.GetKey(distanceField.ChangeModeKey))
+            {
+                currentMode += 1;
+                
+                if(currentMode == 5)
+                {
+                    currentMode = 0;
+                }
+
+                distanceField.Model = (DistanceFieldModels)currentMode;
+                time = 0;
+            }
+
+            distanceField.ElapsedTime = time;
+        }).Run();
+
+        return default;
+    }
+}
---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/SpawnerSystem.cs
@@ -0,0 +1,32 @@
+﻿
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct SpawnerData : IComponentData
+{
+    public Entity ParticlePrefab;
+    public int ParticleCount;
+}
+
+public class SpawnerSystem : JobComponentSystem
+{
+
+    private World _World = World.DefaultGameObjectInjectionWorld;
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.WithStructuralChanges().WithoutBurst().ForEach((Entity entity, in SpawnerData spawner ) =>
+        {
+            _World.EntityManager.Instantiate(spawner.ParticlePrefab, spawner.ParticleCount, Allocator.Temp);
+            _World.EntityManager.DestroyEntity(entity);
+        }).Run();
+
+        return default;
+    }
+}
