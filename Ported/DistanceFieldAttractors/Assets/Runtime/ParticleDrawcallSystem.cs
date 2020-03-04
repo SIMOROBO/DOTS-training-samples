dd85b814f6d62816304a71f11676582ac2a0388d
Yann Alet <yann.alet@eidosmontreal.com>
2020-03-04 13:00:19 -0500

    Rendering entities with IndirectDraw

---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ParticleDrawcallSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class ParticleDrawcallSystem : ComponentSystem
+{
+    EntityQuery m_query;
+
+    protected override void OnCreate()
+    {
+        m_query = GetEntityQuery(typeof(ParticleTransform));
+    }
+
+    protected override void OnUpdate()
+    {
+        var transforms = m_query.ToComponentDataArray<ParticleTransform>(Unity.Collections.Allocator.TempJob);
+        var t = transforms.Reinterpret<Matrix4x4>();
+        var shader = Shader.Find("Instanced/InstancedShader");
+        if (shader != null)
+        {
+            Material material = new Material(shader);
+            Mesh mesh = PrimitiveHelper.GetPrimitiveMesh(PrimitiveType.Cube);
+            
+            if (material != null && mesh != null && t != null)
+            {
+                var _drawcall = DrawCallGenerator.GetDrawCall(mesh, material, t);
+                Graphics.DrawMeshInstancedIndirect(_drawcall.Mesh, _drawcall.SubMeshIndex, _drawcall.Material, _drawcall.Bounds, _drawcall.ArgsBuffer);
+            }
+        }
+        t.Dispose();
+
+    }
+}
---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ParticleTransformSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[UpdateBefore(typeof(ParticleDrawcallSystem))]
+public class ParticleTransformSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        // 1 - Initialization
+        // 2 - Sim group
+        // 3 - Presentation group
+
+        Entities.ForEach((Entity entity, ref ParticleTransform particleTransform, in ParticleData particleData, in MaterialData materialData) =>
+        {
+            var m = System.Numerics.Matrix4x4.CreateTranslation(new System.Numerics.Vector3(particleData.Position.x, particleData.Position.y, particleData.Position.z));
+            particleTransform.transform = m;
+            
+        }).Run();
+        return default;
+    }
+}
