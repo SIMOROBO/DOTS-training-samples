beaa26a875952ee9649e6367f93b205ceff9d544
Simon Paradis Tremblay <paradissimo@gmail.com>
2020-03-04 00:33:06 -0500

    Added Random.InsideUnitSphere as color in ParticleGenerator

--- Ported/DistanceFieldAttractors/Assets/Runtime/ShaderGraph/ParticlesGenerator.cs
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ShaderGraph/ParticlesGenerator.cs
@@ -31,46 +31,31 @@ public class ParticlesGenerator : MonoBehaviour
 
         var newVertices = new Vector3[newVertexCount];
         var newNormals = new Vector3[newVertexCount];
+        var newColors = new Color[newVertexCount];
         var newTriangles = new int[newTrianglesCount];
 
         for (var i = 0; i < m_ParticleCount; i++)
         {
+            var pos = Random.insideUnitSphere;
+
             for (var j = 0; j < originalVertexCount; j++)
             {
                 var id = i * originalVertexCount + j;
                 newVertices[id] = originalVertices[j];
                 newNormals[id] = originalNormals[j];
-                //Debug.Log("Vertex "+ id +" "+ newVertices[id]);
+                newColors[id] = new Color(pos.x, pos.y, pos.z, 0);
             }
 
             for (var k = 0; k < originalTrianglesCount; k++)
             {
                 var id = i * originalTrianglesCount + k;
                 newTriangles[id] = originalTriangles[k] + i * originalVertexCount;
-                //Debug.Log("Index " + id + " " + newTriangles[id]);
             }
         }
 
         mesh.vertices = newVertices;
         mesh.normals = newNormals;
+        mesh.colors = newColors;
         mesh.triangles = newTriangles;
-
-        /*
-        var layout = new[]
-                {
-            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
-            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float16, 2),
-            new VertexAttributeDescriptor(VertexAttribute.Color, VertexAttributeFormat.UInt8, 3),
-        };
-        var vertexCount = 24 * m_ParticleCount;
-        mesh.SetVertexBufferParams(vertexCount, layout);
-
-        // set vertex data
-        var verts = new NativeArray<ExampleVertex>(vertexCount, Allocator.Temp);
-
-        // ... fill in vertex array data here...
-
-        mesh.SetVertexBufferData(verts, 0, 0, vertexCount);
-        */
     }
 }
