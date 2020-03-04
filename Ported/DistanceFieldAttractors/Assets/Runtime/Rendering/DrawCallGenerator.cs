d5a077e7c17b65179e52f82803ca75fd1e57ad5f
Yann <yann.alet@gmail.com>
2020-03-03 21:55:54 -0500

    Added ParticleRenderer for single drawcall

---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/Rendering/DrawCallGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using UnityEngine;
+ 
+public static class DrawCallGenerator
+{
+    public static ParticleDrawCall GetDrawCall(Mesh instanceMesh, Material material, NativeArray<Matrix4x4> transforms)
+    {
+        ComputeBuffer argsBuffer;
+        ComputeBuffer positionBuffer;
+        GetBuffers(instanceMesh, transforms, out argsBuffer, out positionBuffer);
+        material.SetBuffer("transformBuffer", positionBuffer);
+        ParticleDrawCall drawcall = new ParticleDrawCall();
+        drawcall.Mesh = instanceMesh;
+        drawcall.SubMeshIndex = 0;
+        drawcall.Material = material;
+        drawcall.Bounds = new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f));
+        drawcall.PositionBuffer = positionBuffer;
+        drawcall.ArgsBuffer = argsBuffer;        
+        return drawcall;
+    }
+
+    private static void GetBuffers(Mesh instanceMesh, NativeArray<Matrix4x4> transforms, out ComputeBuffer argsBuffer, out ComputeBuffer positionBuffer)
+    {
+        positionBuffer = new ComputeBuffer(transforms.Length, 16 * 4);
+
+        positionBuffer.SetData(transforms);
+
+        // Indirect args
+        uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+        args[0] = (uint)instanceMesh.GetIndexCount(0);
+        args[1] = (uint)transforms.Length;
+        args[2] = (uint)instanceMesh.GetIndexStart(0);
+        args[3] = (uint)instanceMesh.GetBaseVertex(0);
+
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(args);
+    }
+}
---
+++ Ported/DistanceFieldAttractors/Assets/Runtime/Rendering/ParticleRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class ParticleRenderer : MonoBehaviour
+{
+    public Mesh _mesh;
+    public Material _material;
+    private ParticleDrawCall _drawcall;
+
+    void Update()
+    {
+        DrawParticles();
+    }
+
+    private void DrawParticles()
+    {
+        if (_drawcall == null && _mesh != null && _material != null)
+        {
+            NativeArray<Matrix4x4> transforms = GetTransforms(10000);
+            _drawcall = DrawCallGenerator.GetDrawCall(_mesh, _material, transforms);
+        }
+
+        if (_drawcall != null)
+        {
+            Graphics.DrawMeshInstancedIndirect(_drawcall.Mesh, _drawcall.SubMeshIndex, _drawcall.Material, _drawcall.Bounds, _drawcall.ArgsBuffer);
+        }
+    }
+
+    private NativeArray<Matrix4x4> GetTransforms(int instanceCount)
+    {
+        int instancesPerRow = System.Convert.ToInt32(System.Math.Pow(instanceCount, (1.0 / 3.0)));
+        instanceCount = instancesPerRow * instancesPerRow * instancesPerRow;
+        Matrix4x4[] transformsArray = new Matrix4x4[instanceCount];
+        int spacing = 3;
+        for (int x = 0; x < instancesPerRow; x++)
+        {
+            for (int y = 0; y < instancesPerRow; y++)
+            {
+                for (int z = 0; z < instancesPerRow; z++)
+                {
+                    int i = (x * (instancesPerRow * instancesPerRow)) + y * instancesPerRow + z;
+                    transformsArray[i] = Matrix4x4.Translate(new Vector3(x, y, z) * spacing);
+                }
+            }
+        }
+
+        var transforms = new NativeArray<Matrix4x4>(transformsArray, Allocator.Temp);
+        return transforms;
+    }
+
+    void OnDisable()
+    {
+        if(_drawcall != null)
+        {
+            _drawcall.Release();
+            _drawcall = null;
+        }
+    }
+}
