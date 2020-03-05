e672f2f1d274e92bddce5fd1d576f083d7034130
Yan Verde <yverde@eidosmontreal.com>
2020-03-05 09:39:42 -0500

    Convert if to switch

--- Ported/DistanceFieldAttractors/Assets/Runtime/ParticleSimulationSystem.cs
+++ Ported/DistanceFieldAttractors/Assets/Runtime/ParticleSimulationSystem.cs
@@ -49,93 +49,108 @@ public class ParticleSimulationSystem : JobComponentSystem
 
             float distance = float.MaxValue;
             normal = float3.zero;
-            if (Model == DistanceFieldModels.Metaballs)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    float orbitRadius = i * 0.5f + 2f;
-                    float angle1 = Time * 4f * (1f + i * 0.1f);
-                    float angle2 = Time * 4f * (1.2f + i * 0.117f);
-                    float angle3 = Time * 4f * (1.3f + i * 0.1618f);
-                    float cx = math.cos(angle1) * orbitRadius;
-                    float cy = math.sin(angle2) * orbitRadius;
-                    float cz = math.sin(angle3) * orbitRadius;
 
-                    float newDist = SmoothMin(distance, Sphere(x - cx, y - cy, z - cz, 2f), 2f);
-                    if (newDist < distance)
+            switch (Model)
+            {
+                case DistanceFieldModels.Metaballs:
+
+                    for (int i = 0; i < 5; i++)
                     {
-                        normal = new float3(x - cx, y - cy, z - cz);
-                        distance = newDist;
-                    }
-                }
-            }
-            else if (Model == DistanceFieldModels.SpinMixer)
-            {
-                for (int i = 0; i < 6; i++)
-                {
-                    float orbitRadius = (i / 2 + 2) * 2;
-                    float angle = Time * 20f * (1f + i * 0.1f);
-                    float cx = math.cos(angle) * orbitRadius;
-                    float cy = math.sin(angle);
-                    float cz = math.sin(angle) * orbitRadius;
+                        float orbitRadius = i * 0.5f + 2f;
+                        float angle1 = Time * 4f * (1f + i * 0.1f);
+                        float angle2 = Time * 4f * (1.2f + i * 0.117f);
+                        float angle3 = Time * 4f * (1.3f + i * 0.1618f);
+                        float cx = math.cos(angle1) * orbitRadius;
+                        float cy = math.sin(angle2) * orbitRadius;
+                        float cz = math.sin(angle3) * orbitRadius;
 
-                    float newDist = Sphere(x - cx, y - cy, z - cz, 2f);
-                    if (newDist < distance)
+                        float newDist = SmoothMin(distance, Sphere(x - cx, y - cy, z - cz, 2f), 2f);
+                        if (newDist < distance)
+                        {
+                            normal = new float3(x - cx, y - cy, z - cz);
+                            distance = newDist;
+                        }
+                    }
+
+                    break;
+
+                case DistanceFieldModels.SpinMixer:
+                    for (int i = 0; i < 6; i++)
                     {
-                        normal = new float3(x - cx, y - cy, z - cz);
-                        distance = newDist;
+                        float orbitRadius = (i / 2 + 2) * 2;
+                        float anglea = Time * 20f * (1f + i * 0.1f);
+                        float cx = math.cos(anglea) * orbitRadius;
+                        float cy = math.sin(anglea);
+                        float cz = math.sin(anglea) * orbitRadius;
+
+                        float newDist = Sphere(x - cx, y - cy, z - cz, 2f);
+
+                        if (newDist < distance)
+                        {
+                            normal = new float3(x - cx, y - cy, z - cz);
+                            distance = newDist;
+                        }
                     }
-                }
-            }
-            else if (Model == DistanceFieldModels.SpherePlane)
-            {
-                float sphereDist = Sphere(x, y, z, 5f);
-                float3 sphereNormal = math.normalize(new float3(x, y, z));
 
-                float planeDist = y;
-                float3 planeNormal = new float3(0f, 1f, 0f);
+                    break;
 
-                float t = math.sin(Time * 8f) * 0.4f + 0.4f;
-                distance = math.lerp(sphereDist, planeDist, t);
-                normal = math.lerp(sphereNormal, planeNormal, t);
-            }
-            else if (Model == DistanceFieldModels.SphereField)
-            {
-                float spacing = 5f + math.sin(Time * 5f) * 2f;
-                x += spacing * 0.5f;
-                y += spacing * 0.5f;
-                z += spacing * 0.5f;
-                x -= math.floor(x / spacing) * spacing;
-                y -= math.floor(y / spacing) * spacing;
-                z -= math.floor(z / spacing) * spacing;
-                x -= spacing * 0.5f;
-                y -= spacing * 0.5f;
-                z -= spacing * 0.5f;
-                distance = Sphere(x, y, z, 5f);
-                normal = new float3(x, y, z);
-            }
-            else if (Model == DistanceFieldModels.FigureEight)
-            {
-                float ringRadius = 4f;
-                float flipper = 1f;
-                if (z < 0f)
-                {
-                    z = -z;
-                    flipper = -1f;
-                }
-                float3 point = math.normalize(new float3(x, 0f, z - ringRadius)) * ringRadius;
-                float angle = math.atan2(point.z, point.x) + Time * 8f;
-                point += new float3(0f, 0f, 1f) * ringRadius;
-                normal = new float3(x - point.x, y - point.y, (z - point.z) * flipper);
-                float wave = math.cos(angle * flipper * 3f) * 0.5f + 0.5f;
-                wave *= wave * 0.5f;
-                distance = math.sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z) - (0.5f + wave);
-            }
-            else if (Model == DistanceFieldModels.PerlinNoise)
-            {
-                float perlin = noise.cnoise(new float2(x * 0.2f, z * 0.2f));
-                distance = y - perlin * 6f;
-                normal = math.up();
+                case DistanceFieldModels.SpherePlane:
+
+                    float sphereDist = Sphere(x, y, z, 5f);
+                    float3 sphereNormal = math.normalize(new float3(x, y, z));
+
+                    float planeDist = y;
+                    float3 planeNormal = new float3(0f, 1f, 0f);
+
+                    float t = math.sin(Time * 8f) * 0.4f + 0.4f;
+                    distance = math.lerp(sphereDist, planeDist, t);
+                    normal = math.lerp(sphereNormal, planeNormal, t);
+
+                    break;
+
+                case DistanceFieldModels.SphereField:
+
+                    float spacing = 5f + math.sin(Time * 5f) * 2f;
+                    x += spacing * 0.5f;
+                    y += spacing * 0.5f;
+                    z += spacing * 0.5f;
+                    x -= math.floor(x / spacing) * spacing;
+                    y -= math.floor(y / spacing) * spacing;
+                    z -= math.floor(z / spacing) * spacing;
+                    x -= spacing * 0.5f;
+                    y -= spacing * 0.5f;
+                    z -= spacing * 0.5f;
+                    distance = Sphere(x, y, z, 5f);
+                    normal = new float3(x, y, z);
+
+                    break;
+
+                case DistanceFieldModels.FigureEight:
+
+                    float ringRadius = 4f;
+                    float flipper = 1f;
+                    if (z < 0f)
+                    {
+                        z = -z;
+                        flipper = -1f;
+                    }
+                    float3 point = math.normalize(new float3(x, 0f, z - ringRadius)) * ringRadius;
+                    float angle = math.atan2(point.z, point.x) + Time * 8f;
+                    point += new float3(0f, 0f, 1f) * ringRadius;
+                    normal = new float3(x - point.x, y - point.y, (z - point.z) * flipper);
+                    float wave = math.cos(angle * flipper * 3f) * 0.5f + 0.5f;
+                    wave *= wave * 0.5f;
+                    distance = math.sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z) - (0.5f + wave);
+
+                    break;
+
+                case DistanceFieldModels.PerlinNoise:
+
+                    float perlin = noise.cnoise(new float2(x * 0.2f, z * 0.2f));
+                    distance = y - perlin * 6f;
+                    normal = math.up();
+
+                    break;
             }
 
             return distance;
@@ -189,7 +204,7 @@ public class ParticleSimulationSystem : JobComponentSystem
             ExteriorColorDist = particleManagerData.ExteriorColorDist,
             ColorStiffness = particleManagerData.ColorStiffness,
         };
-        
+
         var particleHandle = particleSimulationJob.Schedule(this, inputDeps);
         particleHandle.Complete();
 
